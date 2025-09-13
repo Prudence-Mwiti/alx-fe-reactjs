d4d24f721d3bf91874a4b5cf32ee52382d5e5962
Prudence <dencywalker72@gmail.com>
2025-09-14 00:31:13 +0300

    Refactor prop drilling to Context API with UserContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,18 @@
-import Counter from './components/Counter';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div>
-      <h1>My React App</h1>
-      <Counter />
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
 export default App;
+
 
 
 
