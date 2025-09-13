3f2a3d7150b9234d38573ca0f3d300fe70a70bd7
Prudence <dencywalker72@gmail.com>
2025-09-13 23:42:49 +0300

    Milestone: Apply inline CSS styling to components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>© 2025 My Cities App</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
+
