ae6215895268ee1a4208b8b346f62df7d1f33634
Prudence <dencywalker72@gmail.com>
2025-09-13 22:40:01 +0300

    Milestone components

---
+++ alx-react-app/src/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+  return (
+    <main>
+      <ul>
+        <li>Nairobi</li>
+        <li>Paris</li>
+        <li>New York</li>
+      </ul>
+    </main>
+  );
+}
+
+export default MainContent;
