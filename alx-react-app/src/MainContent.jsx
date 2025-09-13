67387d2f9f760a8912179311bf691b15cb3f42f4
Prudence Karimi <dencywalker72@gmail.com>
2025-09-13 22:19:22 +0300

    Update MainContent.jsx

--- alx-react-app/src/MainContent.jsx
+++ alx-react-app/src/MainContent.jsx
@@ -1,1 +1,14 @@
+function MainContent() {
+  return (
+    <main>
+      <ul>
+        <li>Nairobi</li>
+        <li>Paris</li>
+        <li>New York</li>
+      </ul>
+    </main>
+  );
+}
+
+export default MainContent;
 
