e9737eadee1ac3587382be7f5a1dba46efe0d106
Prudence <dencywalker72@gmail.com>
2025-09-14 00:13:35 +0300

    Milestone: Refactor prop drilling to use Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
+
+
