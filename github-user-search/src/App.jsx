774130fbda7473761a307c21c775b807e81ba6fc
Prudence <dencywalker72@gmail.com>
2025-09-20 23:30:09 +0300

    Setup GitHub User Search project with axios and basic structure

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Start building your search app here...</p>
+    </div>
+  )
+}
+
+export default App
+
+
